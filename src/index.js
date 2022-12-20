9772f869ecc51062f20eb7a397010bad845b96b7
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-12-20 18:46:53 +0500

    Added 'displayScores' function by importing it into 'index.js'
    in this commit
    - added 'displayScores' that dynamically displays scores using JavaScript.
    - added a professional documentation.
    - used import/export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-// import _ from 'lodash';
 import './style.css';
+import displayScores from './modules/displayScores';
 
+displayScores();
---
+++ src/modules/displayScores.js
@@ -0,0 +1,14 @@
+const displayScores = () => {
+  const scoresTable = document.querySelector('.scores');
+  scoresTable.innerHTML = '';
+  
+    const inddividualScore = `<li class="individual-score">Name: 92</li>
+    <li class="individual-score">Name: 100</li>
+    <li class="individual-score">Name: 20</li>
+    <li class="individual-score">Name: 58</li>
+    <li class="individual-score">Name: 89</li>`;
+    scoresTable.innerHTML += inddividualScore;
+  scoresTable.appendChild(inddividualScore);
+};
+
+export default displayScores;
