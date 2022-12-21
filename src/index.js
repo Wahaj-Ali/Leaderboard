fd77ef626c924791997257d3e26100c7b5e2c3ed
Wahaj Ali <wahaj.ali96@yahoo.com>
2022-12-21 19:01:02 +0500

    used API to post and get scores for my game "Scrabble"
    in this commit
    - used Leaderboard API to create a new game "scrabble".
    - Used "refresh" button to display the newly added scores.
    - used submit form to post the scores from the inputs of the form.
    - used arrow functions
    - used async and await to consume the API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import './style.css';
 import displayScores from './modules/displayScores.js';
+import { postScores } from './modules/postScores.js';
+import { getScores } from './modules/getScores.js';
 
-displayScores();
+
+const refresh = document.getElementById('refresh');
+const scoresContainer = document.querySelector('.scores');
+
+const form = document.getElementById('form');
+const gameApiId = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/xwmYsmSekdGODUZnA7Hl/scores/';
+
+//post scores
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    postScores(gameApiId);
+});
+
+// get scores
+
+
+
+//display scores on refresh
+
+refresh.addEventListener('click', () => {
+    scoresContainer.innerHTML = '';
+    getScores(gameApiId).then((data) => displayScores(data.result));
+});
+
+window.onload = getScores(gameApiId).then((data) => displayScores(data.result));
---
+++ src/modules/postScores.js
@@ -0,0 +1,20 @@
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+
+const postScores = async (url) => {
+  const result = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: nameInput.value,
+      score: scoreInput.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  nameInput.value = '';
+  scoreInput.value = '';
+  return result.json();
+};
+
+export { postScores }
