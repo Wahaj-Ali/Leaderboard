8138feffd4cec32cea5489c348d63514fac1b3ac
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-26 21:08:16 +0500

    Install linter for local enviornment
    In this commit
    - Add linters and removed errors.

--- src/modules/displayScores.js
+++ src/modules/displayScores.js
@@ -1,7 +1,7 @@
 const displayScores = (scores) => {
   const scoresTable = document.querySelector('.scores');
   scoresTable.innerHTML = '<li class="individual-score h"><p>Player</p> <p>Score</p></li>';
-  scores.sort((a,b)=> b.score - a.score).forEach((score) => {
+  scores.sort((a, b) => b.score - a.score).forEach((score) => {
     const inddividualScore = `<li class="individual-score"><p>${score.user}</p> <p>${score.score}</p></li>`;
     scoresTable.innerHTML += inddividualScore;
   });
